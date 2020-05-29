8f08dfb797fd44fcdb85080cbf5f54d3c5021677
Joseph Ferano <joseph@ferano.io>
2020-05-29 21:48:39 +0700

    Malloc instead of variable length array

--- marks.c
+++ marks.c
@@ -6,6 +6,7 @@ int main(int argc, char * argv[])
 {
     char *marks_filepath;
     char *xdg_data_home;
+    char *b;
 
     if ((xdg_data_home = getenv("XDG_DATA_HOME")) != NULL)
     {
@@ -17,12 +18,10 @@ int main(int argc, char * argv[])
         if ((home_path = getenv("HOME")) != NULL)
         {
             char *share_path = "/.local/share";
-            char b[strlen(home_path) + strlen(share_path)];
-            strcat(b, home_path);
+            b = malloc(strlen(home_path) + strlen(share_path) + 1);
+            strcpy(b, home_path);
             strcat(b, share_path);
             xdg_data_home = b;
-            // printf(home_path);
-            // printf(share_path);
         }
         else
         {
@@ -31,8 +30,9 @@ int main(int argc, char * argv[])
         }
 
     }
-    printf(xdg_data_home);
+    printf("%s\n", xdg_data_home);
 
+    free(b);
     return 0;
 }
 
