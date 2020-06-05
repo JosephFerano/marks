c174a648cf191a77cbc11c79dbb46b3575b08920
Joseph Ferano <joseph@ferano.io>
2020-06-06 03:56:07 +0700

    Adding better home + xdg path handling

--- marks.c
+++ marks.c
@@ -1,38 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+// #include <sys/stat.h>
+// #include <sys/types.h>
+#include <sys/fcntl.h>
+#include <stdbool.h>
 
 int main(int argc, char * argv[])
 {
-    char *marks_filepath;
-    char *xdg_data_home;
-    char *b;
+    const unsigned char PATH_BUF_SIZE = 128;
+    char path[PATH_BUF_SIZE];
+    char *home_path;
+    char *filename = "marks";
 
+    char *xdg_data_home;
     if ((xdg_data_home = getenv("XDG_DATA_HOME")) != NULL)
     {
-        marks_filepath = xdg_data_home;
+        long xdglen = strlen(xdg_data_home);
+        unsigned char xdg_path_size = strlen(filename) + 2; // Extra for slash
+        if (xdglen > PATH_BUF_SIZE - xdg_path_size)
+        {
+            perror("XDG Data Home path is unusually large, exiting...");
+            return 1;
+        }
+        xdg_path_size += xdglen;
+
+        strcpy(path, xdg_data_home);
+        strcat(path, "/");
+        strcat(path, filename);
     }
     else
     {
-        char *home_path;
-        if ((home_path = getenv("HOME")) != NULL)
+        if ((home_path = getenv("HOME")) == NULL)
         {
-            char *share_path = "/.local/share";
-            b = malloc(strlen(home_path) + strlen(share_path) + 1);
-            strcpy(b, home_path);
-            strcat(b, share_path);
-            xdg_data_home = b;
-        }
-        else
-        {
-            perror("Could not find user $HOME directory");
+            perror("Could not find user $HOME environment variable.");
             return 1;
         }
 
-    }
-    printf("%s\n", xdg_data_home);
+        char *marks_filepath = "/.local/share/";
+        long homelen = strlen(home_path);
+        unsigned char path_size = strlen(marks_filepath) + strlen(filename) + 1;
+        if (homelen > PATH_BUF_SIZE - path_size)
+        {
+            perror("HOME path is unusually large, exiting...");
+            return 1;
+        }
+        path_size += homelen;
 
-    free(b);
+        strcpy(path, home_path);
+        strcat(path, marks_filepath);
+        strcat(path, filename);
+    }
+
+
+    int fd = open(path, O_RDWR | O_CREAT, 0644);
+    if (fd == -1)
+    {
+        perror("Error opening/creating file marks file.");
+        return 1;
+    }
+    printf("The file descriptor is: %i", fd);
+
+
     return 0;
 }
 
